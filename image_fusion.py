a681aaf92c52deeef73a70863acf535c58c603e7
Isaac Perez Del Valle <iperezd@unal.edu.co>
2023-11-30 08:59:03 -0500

    cambios
    se agregaron funciones para el procesamiento batch

--- image_fusion.py
+++ image_fusion.py
@@ -11,13 +11,17 @@ def fusion_de_imagenes(directory):
     # The directory variable is where the files are safe
     os.chdir(directory)      #Here we move to the file ubication
 
-    #The object this code is open 5 to 5 images and save them names in lists
-    
+    #The objety this code is open 5 to 5 images and save them names in lists
+    imagenes=os.listdir()
+    for img in imagenes:
+        second_capa= pdb.gimp_file_load_layer(image,secon_capa)
+        pbd,gimp_image_insert_layer(image,second_capa,None,0)
+
     pass
 
 register(
     "image_fusion",
-    "Aqui voy a haxcer un codigo de tal forma que se e pueda fucionar las imagenes en un determinado recuadro con el objetivo de tener las imagenes de istmina",
+    "Aqui voy a haccer un codigo de tal forma que se e pueda fucionar las imagenes en un determinado recuadro con el objetivo de tener las imagenes de istmina",
     "en esta funcion se colocara la direccion de las imagenes de istimna ",
     "Isaac P",
     "Copyright AULA STEM",
@@ -25,7 +29,8 @@ register(
     "<Image>/Filtros/fusion de imagenes",
 
     [
-        (PF_STRING, "directory", "Es la ubicaion donde se encuentran las imagenes en el pc")
+        (PF_STRING, "directory", "Es la ubicacion donde se encuentran las imagenes en el pc")
     ],
-    image_fusion
-)
+    fusion_de_imagenes)
+
+main()
